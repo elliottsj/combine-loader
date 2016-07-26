8efeb5ec8e6787b16800bb3db4a405d05503bb79
Spencer Elliott <spencer@eventmobi.com>
2016-07-26 13:55:54 -0400

    Use eslint-config-airbnb-base

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import loaderUtils from 'loader-utils'
+import loaderUtils from 'loader-utils';
 
-module.exports = function () {}
+module.exports = function () {};
 module.exports.pitch = function (remainingRequest) {
-  this.cacheable()
-  const loaders = loaderUtils.parseQuery(this.query)
+  this.cacheable();
+  const loaders = loaderUtils.parseQuery(this.query);
 
   const keysValues = Object.keys(loaders).map(key => {
     const loader = Array.isArray(loaders[key])
       ? loaders[key].join('!')
-      : loaders[key]
-    return `"${key}": require("-!${loader}!${remainingRequest}")`
-  })
-  return `module.exports = {${keysValues.join(',')}}`
-}
+      : loaders[key];
+    return `"${key}": require("-!${loader}!${remainingRequest}")`;
+  });
+  return `module.exports = {${keysValues.join(',')}}`;
+};
