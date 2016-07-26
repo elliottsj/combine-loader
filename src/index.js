2ba7c5b80ccaefd6edccc8999c1e608627abd5a2
fredaas <fredaas86@gmail.com>
2016-07-26 12:07:46 +0200

    Fix Windows path issue.

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ module.exports = function () {}
 module.exports.pitch = function (remainingRequest) {
   this.cacheable()
   const loaders = loaderUtils.parseQuery(this.query)
+  const validRequest = process.platform === 'win32'
+    ? remainingRequest.replace(/\\/g, '/')
+    : remainingRequest
 
   const keysValues = Object.keys(loaders).map(key => {
     const loader = Array.isArray(loaders[key])
       ? loaders[key].join('!')
       : loaders[key]
-    return `"${key}": require("-!${loader}!${remainingRequest}")`
+    return `"${key}": require("-!${loader}!${validRequest}")`
   })
   return `module.exports = {${keysValues.join(',')}}`
 }
